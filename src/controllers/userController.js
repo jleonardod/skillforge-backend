f39c0d16bb82adc5012a3b38ca8a95d313f88f4c
Leonardo Diaz <jldiaz9623@gmail.com>
2025-05-07 10:48:46 -0500

    Creacion de rutas y controlador para registro y login

---
+++ src/controllers/userController.js
@@ -0,0 +1,46 @@
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const prisma = new PrismaClient()
+
+
+const register = async (req, res) => {
+  const { username, password } = req.body
+  try{
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword
+      }
+    })
+    res.status(201).json({ message: 'Usuario creado exitosamente', user })
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear el usuario', error })
+  }
+}
+
+const login = async (req, res) => {
+  const { username, password } = req.body
+  try{
+    const user = await prisma.user.findUnique({where: {username}})
+    if(!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' })
+    }
+    const validPassword = await bcrypt.compare(password, user.password)
+    if(!validPassword) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' })
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', token })
+  } catch (error) {
+    res.status(500).json({ message: 'Error al iniciar sesión', error })
+  }
+}
+
+module.exports = {
+  register,
+  login
+}
